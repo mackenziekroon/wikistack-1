96f0895a3d2c733eca103ffe4b213f538cb778b5
Mackenzie Kroon <kroonmackenzie@gmail.com>
2020-10-01 15:11:46 -0500

    second switch

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const layout = require("./views/layout");
+const { db, Page, User } = require("./models");
 
-
-
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //app.use(express.urlencoded({extended: true}))
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res, next) => {
+  let layoutFunc = layout("");
+  res.send(layoutFunc);
+});
 
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
-app.get('/', (req, res, next) => {
-    res.send("Hello world")
-}) 
+const PORT = 3000;
+
+const init = async () => {
+  await db.sync({ force: true });
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+};
+
+init();
